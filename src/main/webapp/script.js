8a8595e9fa383cc549954989d54cea563647208c
mnnogo <akireev86348634@gmail.com>
2024-04-12 12:42:54 +0600

    finished POST request

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -21,37 +21,3 @@ function fillTable(cars) {
         tbody.appendChild(newRow);
     });
 }
-
-/* --------------------------------------------------------------- */
-
-document.getElementById("add").addEventListener("click", function () {
-    var xhr = new XMLHttpRequest(),
-    jsonArr,
-    method = "GET",
-    jsonRequestURL = "cars.json";
-    
-    let formData = new FormData(document.getElementById("addition"));
-
-    xhr.open(method, jsonRequestURL);
-    xhr.onreadystatechange = function()
-    {
-        if (xhr.readyState == 4 && xhr.status == 200)
-        {
-            // we convert your JSON into JavaScript object
-            jsonArr = JSON.parse(xhr.responseText);
-
-            // we add new value:
-            jsonArr.push(formData);
-
-            // we send with new request the updated JSON file to the server:
-            xhr.open("POST", jsonRequestURL);
-            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-            
-            xhr.send("jsonTxt=" + JSON.stringify(jsonArr));
-            // but on this place you have to have a server for write updated JSON to the file
-            
-            location.reload();
-        }
-    };
-    xhr.send();
-});
